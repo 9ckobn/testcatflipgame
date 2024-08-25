8dc107e5062691f86b2a3e0afe9d0a845fee615a
Anton <personal.bergman.ton@gmail.com>
2024-08-25 20:26:54 +0300

    test mini app

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const gameName = "testGame";
 const webURL = "www.catflip.run";
 
 const server = express();
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 
 const port = process.env.PORT || 5000;
 
@@ -16,99 +16,113 @@ const SCORE_TOKEN = process.env.SCORE_TOKEN.split(";").map((t) => BigInt(t));
 const queries = {};
 
 function addAllNumbers(number) {
-  const strNumber = number.toString();
+    const strNumber = number.toString();
 
-  if (strNumber.length === 1) return number;
+    if (strNumber.length === 1) return number;
 
-  const numbers = strNumber.split("");
-  var sum = 0;
-  for (var i = 0; i < numbers.length; i++) {
-    sum += parseInt(numbers[i], 10);
-  }
-  return addAllNumbers(sum);
+    const numbers = strNumber.split("");
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        sum += parseInt(numbers[i], 10);
+    }
+    return addAllNumbers(sum);
 }
 
 bot.on("polling_error", console.log);
 
-bot.onText(/\/help/, (msg) =>
-  bot.sendMessage(
-    msg.from.id,
-    "This bot implements a simple game. Say /game if you want to play."
-  )
-);
-bot.onText(/\/start|\/game/, (msg) => bot.sendGame(msg.from.id, gameName));
-bot.on("callback_query", function (query) {
-  if (query.game_short_name !== gameName) {
-    bot.answerCallbackQuery(
-      query.id,
-      "Sorry, '" + query.game_short_name + "' is not available."
-    );
-  } else {
-    queries[query.id] = query;
-    const userId = query.from.id;
-    const nickname = query.from.username;
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const nickname = msg.from.username;
+    const gameurl = `https://${webURL}/index.html?userid=${userId}&usernickname=${nickname}`;
+    bot.sendMessage(chatId, "Tap to play", {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: "Play", web_app: {url: gameurl}}]
+            ]
+        }
+    });
+});
 
-    const gameurl = `https://${webURL}/index.html?id=${query.id}&userid=${userId}&usernickname=${nickname}`;
-    bot.answerCallbackQuery(query.id, { url: gameurl });
-  }
-});
-bot.on("inline_query", function (iq) {
-  bot.answerInlineQuery(iq.id, [
-    { type: "game", id: "0", game_short_name: gameName },
-  ]);
-});
+// bot.onText(/\/help/, (msg) =>
+//   bot.sendMessage(
+//     msg.from.id,
+//     "This bot implements a simple game. Say /game if you want to play."
+//   )
+// );
+// bot.onText(/\/start|\/game/, (msg) => bot.sendGame(msg.from.id, gameName));
+// bot.on("callback_query", function (query) {
+//   if (query.game_short_name !== gameName) {
+//     bot.answerCallbackQuery(
+//       query.id,
+//       "Sorry, '" + query.game_short_name + "' is not available."
+//     );
+//   } else {
+//     queries[query.id] = query;
+//     const userId = query.from.id;
+//     const nickname = query.from.username;
+//
+//     const gameurl = `https://${webURL}/index.html?id=${query.id}&userid=${userId}&usernickname=${nickname}`;
+//     bot.answerCallbackQuery(query.id, { url: gameurl });
+//   }
+// });
+// bot.on("inline_query", function (iq) {
+//   bot.answerInlineQuery(iq.id, [
+//     { type: "game", id: "0", game_short_name: gameName },
+//   ]);
+// });
 
 server.use(express.static(path.join(__dirname, "public")));
 
 server.get("/highscore/:score", function (req, res, next) {
-  if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
+    if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
 
-  const token = SCORE_TOKEN[addAllNumbers(BigInt(req.query.id)) - 1];
+    const token = SCORE_TOKEN[addAllNumbers(BigInt(req.query.id)) - 1];
 
-  let query = queries[req.query.id];
+    let query = queries[req.query.id];
 
-  let options;
-  if (query.message) {
-    options = {
-      chat_id: query.message.chat.id,
-      message_id: query.message.message_id,
-    };
-  } else {
-    options = {
-      inline_message_id: query.inline_message_id,
-    };
-  }
+    let options;
+    if (query.message) {
+        options = {
+            chat_id: query.message.chat.id,
+            message_id: query.message.message_id,
+        };
+    } else {
+        options = {
+            inline_message_id: query.inline_message_id,
+        };
+    }
 
-  // ===== Obfuscation decoding starts =====
-  // Change this part if you want to use your own obfuscation method
-  const obfuscatedScore = BigInt(req.params.score);
+    // ===== Obfuscation decoding starts =====
+    // Change this part if you want to use your own obfuscation method
+    const obfuscatedScore = BigInt(req.params.score);
 
-  const realScore = Math.round(Number(obfuscatedScore / token));
+    const realScore = Math.round(Number(obfuscatedScore / token));
 
-  // If the score is valid
-  if (BigInt(realScore) * token == obfuscatedScore) {
-    // ===== Obfuscation decoding ends =====
-    bot
-      .setGameScore(query.from.id, realScore, options)
-      .then((b) => {
-        return res.status(200).send("Score added successfully");
-      })
-      .catch((err) => {
-        if (
-          err.response.body.description ===
-          "Bad Request: BOT_SCORE_NOT_MODIFIED"
-        ) {
-          return res
-            .status(204)
-            .send("New score is inferior to user's previous one");
-        } else {
-          return res.status(500);
-        }
-      });
-    return;
-  } else {
-    return res.status(400).send("Are you cheating ?");
-  }
+    // If the score is valid
+    if (BigInt(realScore) * token == obfuscatedScore) {
+        // ===== Obfuscation decoding ends =====
+        bot
+            .setGameScore(query.from.id, realScore, options)
+            .then((b) => {
+                return res.status(200).send("Score added successfully");
+            })
+            .catch((err) => {
+                if (
+                    err.response.body.description ===
+                    "Bad Request: BOT_SCORE_NOT_MODIFIED"
+                ) {
+                    return res
+                        .status(204)
+                        .send("New score is inferior to user's previous one");
+                } else {
+                    return res.status(500);
+                }
+            });
+        return;
+    } else {
+        return res.status(400).send("Are you cheating ?");
+    }
 });
 
 server.listen(port);
